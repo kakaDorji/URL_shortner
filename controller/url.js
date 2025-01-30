a5deadd56a5e548023da5bb01d2bc3c50bd13555
@karso22 <12190010.gcit@rub.edu.bt>
2025-01-30 13:32:29 +0600

    make dynamic port

--- controller/url.js
+++ controller/url.js
@@ -1,19 +1,23 @@
 const shortid=require('shortid');
 const URL=require("../models/url");
-async function handleGenerateNewShortURL(req,res){
-  const body=req.body;
-  if(!body.url) return res.status(400).json({error:"url is required"});   
-  const shortId=shortid();
+async function handleGenerateNewShortURL(req, res) {
+  const body = req.body;
+  if (!body.url) return res.status(400).json({ error: "URL is required" });
+
+  const shortId = shortid();
   await URL.create({
-    shortId:shortId,
-    redirectURL:body.url,
-    visitHistory:[],
-    createdBy:req.user._id,
+    shortId: shortId,
+    redirectURL: body.url,
+    visitHistory: [],
+    createdBy: req.user._id,
   });
 
-res.render("home",{
-  id:shortId
-})
+  const baseUrl = req.protocol + '://' + req.get('host'); // Dynamically get base URL
+
+  res.render("home", {
+    id: shortId,
+    baseUrl: baseUrl, // Pass the base URL to the template
+  });
 }
 
 async function handleGetAnalytics(req,res){
